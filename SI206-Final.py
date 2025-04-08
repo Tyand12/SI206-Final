c2e6d5273762e664f6793e09f888bee1438985ee
Jolie Oleshansky <jolieoleshansky@Jolies-MacBook-Pro-3.local>
2025-04-08 11:09:58 -0400

    Jolie name main

--- SI206-Final.py
+++ SI206-Final.py
@@ -110,5 +110,15 @@ def store_weather_data(max_new=20, total_limit=100):
 
         else:
             break
-        
+
     conn.close()
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    create_weather_table()
+    store_weather_data(20, 100)
